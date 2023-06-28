c23d5c87e600b8fb11544a640d1b1688bac5518a
Tkfrnfl <rul64@naver.com>
2023-06-29 02:07:26 +0900

    add cols2 timetalbe

--- src/component/timeFirst/body.tsx
+++ src/component/timeFirst/body.tsx
@@ -17,16 +17,6 @@ import "../../css/index.css";
 const today = new Date();
 const todayDefault = new Date();
 
-const formatCaption: DateFormatter = (month, options) => {
-	return (
-		<>
-			<span>{today.getFullYear()}년 &nbsp;</span>
-
-			{format(month, "LLLL", { locale: options?.locale })}
-		</>
-	);
-};
-
 export default () => {
 	const [screenSize, setScreenSize] = useState(getCurrentDimension());
 	const [showNotice, setShowNotice] = useState(true);
@@ -34,7 +24,15 @@ export default () => {
 	const noticeClose = () => {
 		setShowNotice(false);
 	};
+	const formatCaption: DateFormatter = (month, options) => {
+		return (
+			<>
+				<span>{today.getFullYear()}년 &nbsp;</span>
 
+				{format(month, "LLLL", { locale: options?.locale })}
+			</>
+		);
+	};
 	function getCurrentDimension(): any {
 		return {
 			width: window.innerWidth,
@@ -65,8 +63,129 @@ export default () => {
 		to: addDays(today, 6 - today.getDay()),
 	};
 	const [range, setRange] = useState<DateRange | undefined>(defaultSelected);
-	const onClickRange = (props: any) => {
+
+	const hours: string[] = [
+		"자정0시",
+		"새벽1시",
+		"새벽2시",
+		"새벽3시",
+		"오전5시",
+		"오전6시",
+		"오전7시",
+		"오전8시",
+		"오전9시",
+		"오전10시",
+		"오전11시",
+		"정오12시",
+		"오후13시",
+		"오후14시",
+		"저녁19시",
+		"저녁20시",
+		"저녁21시",
+		"저녁22시",
+		"저녁23시",
+	];
+	const hourList: JSX.Element[] = hours.map((hour, index) => {
+		return (
+			<div key={index} className="row">
+				<div className="relative w-[70px] h-[30px]">
+					<div className="absolute right-[1px] slot-border"></div>
+				</div>
+
+				<div className="relative w-[70px] h-[30px]">
+					<div className="-mt-4 slot-time">{hour}</div>
+					<div className="-mt-4 absolute right-[1px] slot-border-dashed"></div>
+				</div>
+			</div>
+		);
+	});
+	const hoursNumber: string[] = [
+		"0",
+		"1",
+		"2",
+		"3",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9",
+		"10",
+		"11",
+		"12",
+		"13",
+		"14",
+		"19",
+		"20",
+		"21",
+		"22",
+		"23",
+	];
+	const hourNumberList: JSX.Element[] = hoursNumber.map((hour, index) => {
+		return (
+			<div key={index} className="row">
+				<div className="relative w-[10px] h-[30px]">
+					{hour}
+				</div>
+			</div>
+		);
+	});
+	const date: string[] = ["일", "월", "화", "수", "목", "금", "토"];
+	const dateList: JSX.Element[] = date.map((date, index) => {
+		return (
+			<div key={index} className="row">
+				<div>{date}</div>
+				<div>{range!.from!.getDate() + index}</div>
+				<div>{hourNumberList}</div>
+			</div>
+		);
+	});
+
+	const onClickToday = () => {
+		const today = new Date();
+		setRange({
+			from: addDays(today, today.getDay() * -1),
+			to: addDays(today, 6 - today.getDay()),
+		});
+	};
+	const onClickBefore = () => {
+		if (addDays(range!.to!, -7) > today) {
+			setRange({
+				from: addDays(range!.from!, -7),
+				to: addDays(range!.to!, -7),
+			});
+			if (addDays(range!.to!, -7).getMonth() < originMonth!.getMonth()) {
+				const element: HTMLElement = document.querySelector(
+					'[aria-label="Go to previous month"]',
+				) as HTMLElement;
+				if (element) {
+					element.click();
+				}
+			}
+		}
+	};
+	const onClickNext = () => {
+		setRange({
+			from: addDays(range!.from!, 7),
+			to: addDays(range!.to!, 7),
+		});
+		if (addDays(range!.from!, 7).getMonth() > originMonth!.getMonth()) {
+			const element: HTMLElement = document.querySelector(
+				'[aria-label="Go to next month"]',
+			) as HTMLElement;
+			if (element) {
+				element.click();
+			}
+		}
+	};
+	const onMonthChange = (props: Date) => {
 		console.log(props);
+		setOriginMonth(props);
+	};
+	const [originMonth, setOriginMonth] = useState<Date | undefined>(
+		defaultSelected.from,
+	);
+
+	const onClickRange = (props: any) => {
 		if (props.to == undefined) {
 			setRange({
 				from: addDays(props.from, props.from.getDay() * -1),
@@ -114,6 +233,9 @@ export default () => {
 						onSelect={onClickRange}
 						disabled={{ before: today }}
 						showOutsideDays
+						onMonthChange={(props) => {
+							onMonthChange(props);
+						}}
 						fromMonth={today}
 						locale={ko}
 						formatters={{ formatCaption }}
@@ -189,9 +311,12 @@ export default () => {
 				<div className="flex h-[70px] items-center pl-[20px]">
 					<button
 						className="appearance-none box-border focus:outline-none cursor-pointer"
+						onClick={() => {
+							onClickToday();
+						}}
 					>
 						<div
-							className="inline-block font-normal text-center text-h4 leading-snug box-border btn-sm bg-white chip-md text-purple-500 border border-purple-500 hover:bg-opacity-80"
+							className="inline-block font-normal text-center text-xs leading-snug box-border btn-sm bg-white chip-md text-purple-500 border border-purple-500 hover:bg-opacity-80 rounded-xl p-1 w-14"
 							//style="padding-top: 3px; padding-bottom: 3px; font-size: 12px; margin-top: 0px; margin-right: 11px;"
 						>
 							오늘
@@ -200,28 +325,34 @@ export default () => {
 					<button
 						//data-ref="btn_previous"
 						className="appearance-none box-border focus:outline-none cursor-pointer"
-						//style="padding-left: 0px; padding-right: 0px;"
+						onClick={() => {
+							onClickBefore();
+						}}
 					>
 						<div
-							className="flex items-center inline-block font-normal text-center text-h4 leading-snug box-border btn-sm bg-white chip-md text-purple-500 border border-purple-500 hover:bg-opacity-80"
+							className="flex items-center inline-block font-normal text-center text-h4 leading-snug box-border btn-sm bg-white chip-md text-purple-500 border border-purple-500 hover:bg-opacity-80 rounded-xl p-1 w-8 ml-3"
 							//style="margin-right: 11px; height: 24px;"
 						>
 							<img
+								className="ml-1"
 								alt="icon_chevron_left_primary"
 								src="https://d38emex6h5e12i.cloudfront.net/new-theme/new-icons/adult/icon/chevron-left/primary.svg"
 							/>
 						</div>
 					</button>
 					<button
-					//	data-ref="btn_next"
+						//	data-ref="btn_next"
 						className="appearance-none box-border focus:outline-none cursor-pointer"
-					//	style="padding-left: 0px; padding-right: 0px;"
+						onClick={() => {
+							onClickNext();
+						}}
 					>
 						<div
-							className="flex items-center inline-block font-normal text-center text-h4 leading-snug box-border btn-sm bg-white chip-md text-purple-500 border border-purple-500 hover:bg-opacity-80"
-						//	style="height: 24px;"
+							className="flex items-center inline-block font-normal text-center text-h4 leading-snug box-border btn-sm bg-white chip-md text-purple-500 border border-purple-500 hover:bg-opacity-80 rounded-xl p-1 w-8 ml-3"
+							//	style="height: 24px;"
 						>
 							<img
+								className="ml-1"
 								alt="icon_chevron_right_primary"
 								src="https://d38emex6h5e12i.cloudfront.net/new-theme/new-icons/adult/icon/chevron-right/primary.svg"
 							/>
@@ -232,10 +363,14 @@ export default () => {
 						<span className="text-sBody text-[12px] text-black">
 							Asia/Seoul
 						</span>
-						<a className="text-h6 ml-[8px] cursor-pointer text-gray-500">
+						<a className="text-xs ml-[8px] cursor-pointer text-gray-500">
 							타임존 변경
 						</a>
 					</div>
+				</div>
+				<div className="flex">
+					<div className="mt-16">{hourList}</div>
+					<div className="flex">{dateList}</div>
 				</div>
 			</SlotCols>
 			<div className="min-w-[370px] fixed right-0 bg-white h-[300px]">3</div>
